4e6cc899344191427320f045699b56ec2787ab14
Seth Wiggins <bswigg17@gmail.com>
2022-04-30 10:01:08 -0400

    inital

--- public/main.js
+++ public/main.js
@@ -1,5 +1,69 @@
 // Focus div based on nav button click
 
+const handlesingleflip = async () => {
+    const res = await fetch("/app/flip/", {
+        method: 'GET', 
+        headers: {
+            'Content-Type': 'text/json'
+        }
+    }); 
+    const data = await res.json();
+    document.getElementById('single-result').innerHTML = data.flip;
+    if (data.flip == 'heads') {
+        document.getElementById('single-result-img').src = "./assets/img/heads.png";  
+    }  
+    else {
+        document.getElementById('single-result-img').src = "./assets/img/tails.png";  
+    }
+    
+}
+
+const handlemultipleflips = async () => {
+    const count = document.getElementById('multiple-flip-input').value
+    console.log(count)
+    const res = await fetch("/app/flip/coins/", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }, 
+        body: JSON.stringify({number: count})
+    }); 
+    const data = await res.json(); 
+    const heads = document.getElementById('heads');
+    const tails = document.getElementById('tails');
+    heads.style = `--size: calc( ${data.summary.heads / data.raw.length})`
+    tails.style = `--size: calc( ${data.summary.tails / data.raw.length})`
+}
+
+
+const handleclick = (id) => {
+    const page = document.getElementsByClassName(id)[0]; 
+    if (!page.className.includes('show')) {
+        const current = document.getElementsByClassName("show")[0];
+        current.classList.remove("show");
+        document.getElementById(id).classList.add('highlight');
+        document.getElementById(current.className).classList.remove('highlight');
+        current.classList.add("hidden");
+        page.classList.add("show");
+        page.classList.remove("hidden"); 
+    }
+}
+
+
+const addListenersToNavigation = () => {
+    const buttons = document.getElementsByClassName('nav');
+    Array.from(buttons).forEach(element => {
+        element.addEventListener("click", () => handleclick(element.id)); 
+    })
+    // buttons.array.forEach(button => {
+    //     button.addEventListener("click", () => handleClick(button.id))
+    // });
+}
+
+window.onload = () => {
+    addListenersToNavigation(); 
+}
+
 // Flip one coin and show coin image to match result when button clicked
 
 // Flip multiple coins and show coin images in table as well as summary results
---
+++ src/coinflip.js
@@ -0,0 +1,86 @@
+/** Coin flip functions 
+ * This module will emulate a coin flip given various conditions as parameters as defined below
+ */
+
+/** Simple coin flip
+ * 
+ * Write a function that accepts no parameters but returns either heads or tails at random.
+ * 
+ * @param {*}
+ * @returns {string} 
+ * 
+ * example: coinFlip()
+ * returns: heads
+ * 
+ */
+
+ function coinFlip() {
+    return (Math.floor(Math.random() * 2)) ? 'heads' : 'tails'; 
+  }
+  
+  /** Multiple coin flips
+   * 
+   * Write a function that accepts one parameter (number of flips) and returns an array of 
+   * resulting "heads" or "tails".
+   * 
+   * @param {number} flips 
+   * @returns {string[]} results
+   * 
+   * example: coinFlips(10)
+   * returns:
+   *  [
+        'heads', 'heads',
+        'heads', 'tails',
+        'heads', 'tails',
+        'tails', 'heads',
+        'tails', 'heads'
+      ]
+   */
+  
+  function coinFlips(flips) {
+    let flips_array = new Array(); 
+    for (let i=0; i<flips; i++) {
+      flips_array.push(coinFlip()); 
+    }
+    return flips_array
+  }
+  
+  /** Count multiple flips
+   * 
+   * Write a function that accepts an array consisting of "heads" or "tails" 
+   * (e.g. the results of your `coinFlips()` function) and counts each, returning 
+   * an object containing the number of each.
+   * 
+   * example: conutFlips(['heads', 'heads','heads', 'tails','heads', 'tails','tails', 'heads','tails', 'heads'])
+   * { tails: 5, heads: 5 }
+   * 
+   * @param {string[]} array 
+   * @returns {{ heads: number, tails: number }}
+   */
+  
+  function countFlips(array) {
+    return {heads: array.filter(x => x === 'heads').length,
+            tails: array.filter(x => x === 'tails').length}
+  }
+  
+  /** Flip a coin!
+   * 
+   * Write a function that accepts one input parameter: a string either "heads" or "tails", flips a coin, and then records "win" or "lose". 
+   * 
+   * @param {string} call 
+   * @returns {object} with keys that are the input param (heads or tails), a flip (heads or tails), and the result (win or lose). See below example.
+   * 
+   * example: flipACoin('tails')
+   * returns: { call: 'tails', flip: 'heads', result: 'lose' }
+   */
+  
+  function flipACoin(call) {
+    const a_flip = coinFlip(); 
+    return { call: call, flip: a_flip, result: (a_flip) === call ? 'win': 'lose'}; 
+  }
+  /** Export 
+   * 
+   * Export all of your named functions
+  */
+  
+module.exports =  { coinFlip, coinFlips, countFlips, flipACoin }
